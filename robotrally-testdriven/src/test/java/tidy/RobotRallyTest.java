95e62bb270e42e950e5035a796a64b67ad3c7dee
karlfindhansen <71121687+karlfindhansen@users.noreply.github.com>
2022-05-01 20:41:23 +0200

    Update RobotRallyTest.java

--- robotrally-testdriven/src/test/java/tidy/RobotRallyTest.java
+++ robotrally-testdriven/src/test/java/tidy/RobotRallyTest.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/featureFiles")
+@CucumberOptions(features="src/test/resources")
 
 
 public class RobotRallyTest {
